9892a5260aa284a3ed52ea6e6f80db7d8977145d
SMshoukat <smshoukat11@gmail.com>
2024-05-11 09:48:46 +0500

    added

---
+++ app.js
@@ -0,0 +1,30 @@
+// var emty = "";
+// function range(){
+//     var len = document.getElementById("length").value
+//     document.getElementById("length-value").innerHTML = len
+// }
+    
+//     // symbols...............!!
+//     var simbol = document.getElementById("specialsCb").checked
+//     // small.................!!
+//     var small = document.getElementById("lowercaseCb").checked
+//     // capitals..............!!
+//     var cap = document.getElementById("uppercaseCb").checked
+//     // number................!!
+//     var count = document.getElementById("digitsCb").checked
+
+//     if (simbol == true){
+//         emty += "!@#$%^&*()_"
+//     }
+//    else if (small == true){
+//         emty += "asdfghjklqwertyuiopxcvbnm"
+//     }
+//    else if (cap == true){
+//         emty += "ASDFGHJKLQWERTYUIOPZXCVBNM"
+//     }
+//    else if (count == true){
+//         emty += "1234567890"
+//     }
+
+//     emty = math.floor(math.random() * emty.length)
+     
